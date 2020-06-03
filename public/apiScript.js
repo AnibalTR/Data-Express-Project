c9a7b11111ec60ac24368ca3ea9e0665796d3bf0
CodyAshby222 <60458709+CodyAshby222@users.noreply.github.com>
2020-06-03 13:48:15 -0600

    info on database/api

--- public/apiScript.js
+++ public/apiScript.js
@@ -3,10 +3,16 @@ let url = "http://localhost:3000/api";
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-    document.getElementById(
-      "json_holder"
-    ).innerHTML = `${data[1].wyrQ1}, ${data[1].age}, ${data[1].species}`;
+    //below is how to get total of people in database
+    console.log(data.length);
+    let index;
+    for (index = 0; index < data.length; index++) {
+      console.log(data[index].wyrQuestion1);
+    }
+    // console.log(data);
+    // document.getElementById(
+    //   "json_holder"
+    // ).innerHTML = `${data[1].wyrQ1}, ${data[1].age}, ${data[1].species}`;
     //use the section above to update canvas graph
   })
   .catch((err) => console.log(err));
